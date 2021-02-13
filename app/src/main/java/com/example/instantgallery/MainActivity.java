4d6e4b9977ae4d9daad6fe1ba1795986e551abab
Admin <zhout17@mytru.ca>
2021-02-12 23:52:29 -0800

    redesign logic of gallery gridview

--- app/src/main/java/com/example/instantgallery/MainActivity.java
+++ app/src/main/java/com/example/instantgallery/MainActivity.java
@@ -2,50 +2,36 @@ package com.example.instantgallery;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
-    private final String image_titles[] = {
-            "Img1",
-            "Img2",
-            "Img3",
-            "Img4",
-            "Img5",
-            "Img6",
-            "Img7",
-//            "Img8",
-//            "Img9",
-//            "Img10",
-//            "Img11",
-//            "Img12",
-//            "Img13",
-    };
-    private final Integer image_ids[] = {
-            R.drawable.img1,
-            R.drawable.img2,
-            R.drawable.img3,
-            R.drawable.img4,
-            R.drawable.img5,
-            R.drawable.img6,
-            R.drawable.img7,
-//            R.drawable.img8,
-//            R.drawable.img9,
-//            R.drawable.img10,
-//            R.drawable.img11,
-//            R.drawable.img12,
-//            R.drawable.img13,
-    };
-    RecyclerView recyclerView;
-
+    private GridView gridView;
+    private MyAdapter mAdapter;
+    private List<String> photoList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gridView = (GridView) findViewById(R.id.gridview);
+        mAdapter = (MyAdapter) new MyAdapter(this, photoList);
 
-
+        gridView.setAdapter(mAdapter);
     }
 
+
+    @Override
+    public void onClick(View v)
+    {
+
+    }
 }
